11068f2f72697e6b70ea505b205d0311d62132e6
Dan Forstinger <danforstinger@gmail.com>
2024-08-11 16:08:03 -0400

    Added simple dice system, which was imported from Mythiria project

---
+++ Assets/SimpleDice/Scripts/Core/DiceRoller.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using InnerDriveStudios.DiceCreator;
+using MonsterLove.StateMachine;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Mythiria
+{
+    [System.Serializable]
+    public class DiceRoll
+    {
+        [System.Serializable]
+        public class Frame
+        {
+            public Vector3 position => new Vector3(x, y, z);
+            public Quaternion rotation => new Quaternion(rX, yX, zX, w);
+
+            public Frame(Vector3 pos, Quaternion quat)
+            {
+                x = pos.x;
+                y = pos.y;
+                z = pos.z;
+                rX = quat.x;
+                yX = quat.y;
+                zX = quat.z;
+                w = quat.w;
+            }
+
+            public float x, y, z, rX, yX, zX, w;
+        }
+
+        public List<Frame> frames = new List<Frame>();
+        public DiceType type;
+        public int result = 0;
+
+        public void AddFrame(Vector3 pos, Quaternion quat)
+        {
+            frames.Add(new Frame(pos, quat));
+        }
+    }
+
+    public class DiceRoller : MonoBehaviour
+    {
+        public enum States
+        {
+            Rolled,
+            Idle,
+            AwaitingDestroy
+        }
+
+        public States state => stateMachine.State;
+
+        public DiceRoll roll { get; private set; } = new DiceRoll();
+        public DieSides dice { get; private set; }
+
+        private Camera diceCamera;
+        private StateMachine<States> stateMachine;
+        private DiceType diceType;
+
+        private int recordingIndex;
+        private Action<DiceRoller> replayCompleteCallback;
+
+       [SerializeField] private float minVelocity = 7;
+       [SerializeField] private float maxVelocity = 9;
+       [SerializeField] private float minAngularVelocity = 10;
+       [SerializeField] private float maxAngularVelocity = 12;
+       [SerializeField] private Camera diceCaptureCamera;
+       [SerializeField] private RenderTexture diceCaptureTexture;
+       
+       public bool IsRolling => dice.rigidbody.angularVelocity.magnitude > 0 || dice.rigidbody.velocity.magnitude > 0;
+
+       public Sprite GetTexture()
+       {
+           
+           // snap a pic and send it back.
+           diceCaptureCamera.targetTexture = diceCaptureTexture;
+     
+           
+           var capturePos = dice.transform.position;
+           capturePos.z -= 10;
+           
+           diceCaptureCamera.transform.position = capturePos;
+           
+           var prevRot = dice.transform.rotation;
+
+           Debug.Log("Result: " + roll.result);
+
+           dice.transform.rotation = Quaternion.Euler(0, 0, 0);
+
+           var sideIndex = dice.GetSideIndex(roll.result);
+
+           Debug.Log("Side index: " + sideIndex);
+           dice.transform.rotation = dice.GetWorldRotationFor(sideIndex, Vector3.forward);
+
+           dice.rigidbody.rotation = dice.transform.rotation;
+           
+           diceCaptureCamera.Render();
+
+           Texture2D texture = new Texture2D(diceCaptureTexture.width, diceCaptureTexture.height);
+           Rect rect=new Rect(0, 0, diceCaptureTexture.width, diceCaptureTexture.height);
+           RenderTexture currentRenderTexture = RenderTexture.active;
+           RenderTexture.active = diceCaptureTexture;
+           texture.ReadPixels(rect, 0, 0);
+           texture.Apply();
+           RenderTexture.active = currentRenderTexture;
+           Sprite sprite = Sprite.Create( texture, rect, Vector2.zero );
+           
+          // dice.transform.rotation = prevRot;
+           return sprite;
+       }
+       
+        // This method is used to start receiving input for a local player dice roll
+        public void StartSimulate()
+        {
+            if (dice == null) Debug.LogError("Trying to roll dice, but you haven't yet set the dice to roll!");
+
+            Debug.Log("Started dragging...");
+            roll = new DiceRoll();
+            roll.type = diceType;
+
+            dice.meshRenderer.enabled = false;
+            
+            dice.rigidbody.constraints = RigidbodyConstraints.None;
+
+            var xSign = UnityEngine.Random.Range(0, 100) < 50 ? -1 : 1;
+            var ySign = UnityEngine.Random.Range(0, 100) < 50 ? -1 : 1;
+            var velocity = new Vector2(UnityEngine.Random.Range(minVelocity, maxVelocity) * xSign, UnityEngine.Random.Range(minVelocity, maxVelocity) * ySign);
+            dice.rigidbody.velocity = velocity;
+
+            Vector3 randomSpawnRotation = new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360));
+            dice.transform.rotation = Quaternion.Euler(randomSpawnRotation);
+
+            dice.rigidbody.angularVelocity = randomSpawnRotation * UnityEngine.Random.Range(minAngularVelocity, maxAngularVelocity);
+            dice.rigidbody.velocity = velocity;
+            
+            dice.rigidbody.isKinematic = false;
+        }
+        
+        public void StepSimulate(float deltaTime)
+        {
+            roll.AddFrame(dice.rigidbody.position, dice.rigidbody.rotation);
+        }
+
+        // This method is used to replay a dice roll, such as when one is received over networking.
+        public void ReplayRoll(Action<DiceRoller> completed)
+        {
+            if (dice == null) Debug.LogError("Trying to roll dice, but you haven't yet set the dice to roll!");
+            
+            Debug.Log("Replaying roll...");
+            dice.meshRenderer.enabled = true;
+            dice.rigidbody.isKinematic = true;
+            recordingIndex = 0;
+            replayCompleteCallback = completed;
+            stateMachine.ChangeState(States.Rolled);
+        }
+        
+        private void Awake()
+        {
+            stateMachine = StateMachine<States>.Initialize(this);
+        }
+
+        private void OnDestroy()
+        {
+            if (dice && dice.gameObject) Destroy(dice.gameObject);
+        }
+
+        public void Configure(DieSides dice, Vector3 dicePosition, DiceType type, Camera camera, Color col)
+        {
+            this.dice = dice;
+            this.diceCamera = camera;
+            this.dice.meshRenderer.material.color = col;
+            this.diceType = type;
+            this.dice.transform.position = dicePosition;
+        }
+
+        private void Rolled_Enter()
+        {
+            dice.rigidbody.isKinematic = true;
+            recordingIndex = 0;
+        }
+        
+        private void Rolled_FixedUpdate()
+        {
+            if (recordingIndex >= roll.frames.Count)
+            {
+                var result = dice.GetDieSideMatchInfo().closestMatch.values[0];
+                Debug.Log(string.Format("Animated rolling! Result: {0}", result));
+                stateMachine.ChangeState(States.AwaitingDestroy);
+                return;
+            }    
+            
+            var pair = roll.frames[recordingIndex];
+            
+            dice.rigidbody.position = pair.position;
+            dice.rigidbody.rotation = pair.rotation;
+            recordingIndex++;
+        }
+
+        private void AwaitingDestroy_Enter()
+        {
+            dice.rigidbody.velocity = Vector3.zero;
+            dice.rigidbody.isKinematic = true;
+            replayCompleteCallback?.Invoke(this);
+        }
+    }
+}
---
+++ Assets/SimpleDice/Scripts/Core/DiceRollingController.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using InnerDriveStudios.DiceCreator;
+using MonsterLove.StateMachine;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.UI;
+
+namespace Mythiria
+{
+    public class DiceRollingController : MonoBehaviour
+    {
+        public Camera diceCamera;
+
+        [SerializeField] private DiceRoller rollerPrefab;
+
+        [SerializeField] private DieSides d4;
+        [SerializeField] private DieSides d6;
+        [SerializeField] private DieSides d8;
+        [SerializeField] private DieSides d10;
+        [SerializeField] private DieSides d12;
+        [SerializeField] private DieSides d20;
+    
+        private List<DiceRoller> rolls;
+        
+        private Dictionary<DiceType, DieSides> diceTypeToPrefabMap;
+
+        public void SetRenderTarget(RawImage image)
+        {
+            var texture = new RenderTexture(Screen.width, Screen.height, 32);
+            diceCamera.targetTexture = texture;
+            image.texture = texture;
+        }
+
+        /// Note: For this function, the user is responsible for destroying the roller themselves, otherwise it will stick around.
+        public void ReplayDiceRoll(DiceRoller roller, Color color)
+        {
+            roller.ReplayRoll(roller =>
+            {
+                Debug.Log("Dice roll replay complete!");
+            });
+        }
+        
+        public void SimulateDiceRoll(DiceType diceType, Vector3 spawnPos, Color color, int count, bool autoReplay, System.Action<List<DiceRoller>> callback)
+        {
+            Debug.Log("Rolling dice...");
+            StartCoroutine(ExecSimulateDiceRoll(count, spawnPos, color, diceType, autoReplay, callback));
+        }
+
+        private IEnumerator ExecSimulateDiceRoll(int count,  Vector3 spawnPos, Color color, DiceType diceType, bool autoReplay, System.Action<List<DiceRoller>> callback)
+        {
+            // remove existing dice rollers.
+            if (rolls != null)
+            {
+                for (int r = rolls.Count - 1; r >= 0; --r)
+                {
+                    Destroy(rolls[r].gameObject);
+                }
+            }
+            
+            rolls = new List<DiceRoller>();
+
+            for (int r = 0; r < count; ++r)
+            {
+                var offsetX = -(count / 2) + r;
+                
+                var roller = CreateDiceRoller(diceType, spawnPos + new Vector3(offsetX, 0,0), color);
+                roller.StartSimulate();
+                rolls.Add(roller);
+            }
+
+            Physics.autoSimulation = false;
+      
+            const int maxIter = 5000;
+            int i = 0;
+            float timeStep = 0;
+            diceCamera.enabled = false;
+            
+            while (rolls.Exists(r => r.IsRolling) && i < maxIter)
+            {
+                foreach (var roller in rolls)
+                {
+                    roller.StepSimulate(Time.fixedDeltaTime);
+                }
+
+                Physics.Simulate(Time.fixedDeltaTime);
+                ++i;
+            }
+
+            Physics.autoSimulation = true;
+            
+
+            yield return null;
+            
+            diceCamera.enabled = true;
+
+            foreach (var pair in rolls)
+            {
+                var result = pair.dice.GetDieSideMatchInfo().closestMatch.values[0];
+                Debug.Log(string.Format("Simulated rolling! Result: {0}", result));
+                pair.roll.result = result;
+            }
+            
+            if (autoReplay)
+            {
+                var rollCount = rolls.Count;
+                
+                foreach (var pair in rolls)
+                {
+                    pair.ReplayRoll(roller =>
+                    {
+                        rollCount--;
+                    });
+                }
+                
+                while (rollCount > 0)
+                {
+                    yield return null;
+                }
+
+                yield return null;
+            }
+            
+            callback?.Invoke(rolls);
+        }
+        private void Awake()
+        {
+            rollerPrefab.gameObject.SetActive(false);
+            diceTypeToPrefabMap = new Dictionary<DiceType, DieSides>()
+            {
+                { DiceType.d4, d4 },
+                { DiceType.d6, d6 },
+                { DiceType.d8, d8 },
+                { DiceType.d10, d10 },
+                { DiceType.d12, d12 },
+                { DiceType.d20, d20 },
+            };
+            
+        }
+
+        private DiceRoller CreateDiceRoller(DiceType type,Vector3 dicePosition, Color color)
+        {
+            var prefab = diceTypeToPrefabMap[type];
+
+            var pos = diceCamera.transform.position;
+       
+            var roller = Instantiate(rollerPrefab, pos, Quaternion.identity).GetComponent<DiceRoller>();
+            roller.gameObject.SetActive(true);
+            var dice = Instantiate(prefab.gameObject, pos, Quaternion.identity).GetComponent<DieSides>();
+            
+            roller.Configure(dice, dicePosition, type, diceCamera, color);
+            return roller;
+        }
+    }
+}
